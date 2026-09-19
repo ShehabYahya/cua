431c3b7826c4ae4928fd8b68d386501495c234f3
Shehab Yahya <266881246+ShehabYahya@users.noreply.github.com>
2026-09-19 19:06:46 +0300

    test(cua-driver): adapt direct-mode desktop overview

--- libs/cua-driver/examples/jev-desktop-agent/python/tests/test_compound_workflow.py
+++ libs/cua-driver/examples/jev-desktop-agent/python/tests/test_compound_workflow.py
@@ -67,7 +67,7 @@ class CompoundDriver:
         self.sent = False
         self.counter = 0
 
-    async def desktop_overview(self):
+    async def desktop_overview(self, *, include_screenshot=True):
         return DesktopOverview((), ())
 
     async def has_window(self, app):
